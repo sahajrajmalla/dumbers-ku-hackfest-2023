ea6daab9d0820ea1dbc81fd640aafd300f40ba0d
Sameer Pokhrel <i.amsameer2061@gmail.com>
2023-09-30 01:04:55 +0545

    geo man and map components

---
+++ frontend/src/components/MapComponents.js
@@ -0,0 +1,20 @@
+import { MapContainer, TileLayer } from "react-leaflet";
+import Geoman from "./Geoman";
+
+const Map = () => {
+  const position = [48.863247, 2.350747];
+  const zoomLv = 13;
+
+  return (
+    <MapContainer center={position} zoom={zoomLv} scrollWheelZoom={true}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png"
+        attribution='&copy; OpenStreetMap France | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        maxZoom={20}
+      />
+      <Geoman />
+    </MapContainer>
+  );
+};
+
+export default Map;
