0ef827e790d58cb642e636f574d528d7973c7f81
Diwash Lamichhane <imdiwash.mrprogrammer@gmail.com>
2023-09-30 10:20:07 +0545

    feat: Added stats

--- frontend/src/pages/stats.js
+++ frontend/src/pages/stats.js
@@ -1,9 +1,162 @@
-import React from 'react'
+import React from "react";
 
 function stats() {
   return (
-    <div className='h-[100vh]'>stats</div>
-  )
+    <section className="mt-40 h-[72vh] container mx-auto px-28">
+      <div className="w-full items-strech flex justify-center">
+        <div className="w-3/4 flex gap-10">
+          <img
+            src="https://cdn.pixabay.com/photo/2017/01/05/23/54/bridge-1956646_1280.jpg"
+            className="w-1/2 rounded-lg gap-10"
+          />
+          <div className="p-2 h-full gap-3 flex flex-col justify-between">
+            <div className="flex justify-between">
+              <div>
+                <p className="text-md text-dark font-semibold">
+                  Butwal Signature Bridge
+                </p>
+                <p className="text-[12px] mt-2 flex items-center gap-2 text-primary font-semibold">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="13"
+                    height="14"
+                    viewBox="0 0 13 14"
+                    fill="none"
+                  >
+                    <path
+                      d="M11.5781 7.60935C11.1718 9.6406 9.64028 11.5532 7.49122 11.9806C6.44309 12.1893 5.35582 12.0621 4.38422 11.6169C3.41263 11.1718 2.60624 10.4314 2.07988 9.50133C1.55352 8.57122 1.33403 7.49876 1.45265 6.43665C1.57126 5.37454 2.02195 4.37692 2.74053 3.58585C4.21441 1.96248 6.70309 1.5156 8.73434 2.3281"
+                      stroke="currentColor"
+                      stroke-width="1.5"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                    <path
+                      d="M4.67188 6.79688L6.70312 8.82812L11.5781 3.54688"
+                      stroke="currentColor"
+                      stroke-width="1.5"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                  assessment completed
+                </p>
+              </div>
+              <div className="text-primary text-sm gap-1 h-max px-4 w-max cursor-pointer py-1 rounded-full flex items-center border-2 border-primary">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="16"
+                  viewBox="0 0 20 16"
+                  fill="none"
+                >
+                  <path
+                    d="M16 12H17.125C17.6218 11.9988 18.0979 11.8404 18.4492 11.5593C18.8005 11.2783 18.9985 10.8975 19 10.5V4.5C18.9985 4.10254 18.8005 3.7217 18.4492 3.44065C18.0979 3.1596 17.6218 3.00119 17.125 3H2.875C2.37818 3.00119 1.90212 3.1596 1.55081 3.44065C1.1995 3.7217 1.00148 4.10254 1 4.5V10.5C1.00148 10.8975 1.1995 11.2783 1.55081 11.5593C1.90212 11.8404 2.37818 11.9988 2.875 12H4"
+                    fill="url(#paint0_linear_17_339)"
+                  />
+                  <path
+                    d="M16 12H17.125C17.6218 11.9988 18.0979 11.8404 18.4492 11.5593C18.8005 11.2783 18.9985 10.8975 19 10.5V4.5C18.9985 4.10254 18.8005 3.7217 18.4492 3.44065C18.0979 3.1596 17.6218 3.00119 17.125 3H2.875C2.37818 3.00119 1.90212 3.1596 1.55081 3.44065C1.1995 3.7217 1.00148 4.10254 1 4.5V10.5C1.00148 10.8975 1.1995 11.2783 1.55081 11.5593C1.90212 11.8404 2.37818 11.9988 2.875 12H4"
+                    stroke="white"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M14.86 7H5.14C4.5104 7 4 7.41879 4 7.93538V14.0646C4 14.5812 4.5104 15 5.14 15H14.86C15.4896 15 16 14.5812 16 14.0646V7.93538C16 7.41879 15.4896 7 14.86 7Z"
+                    fill="url(#paint1_linear_17_339)"
+                    stroke="white"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M16 3V2.25C15.9985 1.91878 15.8005 1.60142 15.4492 1.36721C15.0979 1.133 14.6218 1.00099 14.125 1H5.875C5.37818 1.00099 4.90212 1.133 4.55081 1.36721C4.1995 1.60142 4.00148 1.91878 4 2.25V3"
+                    fill="url(#paint2_linear_17_339)"
+                  />
+                  <path
+                    d="M16 3V2.25C15.9985 1.91878 15.8005 1.60142 15.4492 1.36721C15.0979 1.133 14.6218 1.00099 14.125 1H5.875C5.37818 1.00099 4.90212 1.133 4.55081 1.36721C4.1995 1.60142 4.00148 1.91878 4 2.25V3"
+                    stroke="white"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M17 6C17.5523 6 18 5.55228 18 5C18 4.44772 17.5523 4 17 4C16.4477 4 16 4.44772 16 5C16 5.55228 16.4477 6 17 6Z"
+                    fill="url(#paint3_linear_17_339)"
+                  />
+                  <defs>
+                    <linearGradient
+                      id="paint0_linear_17_339"
+                      x1="10"
+                      y1="3"
+                      x2="10"
+                      y2="12"
+                      gradientUnits="userSpaceOnUse"
+                    >
+                      <stop stop-color="#9EF01A" />
+                      <stop offset="0.84375" stop-color="#08DA08" />
+                      <stop offset="1" stop-color="#09DA09" />
+                    </linearGradient>
+                    <linearGradient
+                      id="paint1_linear_17_339"
+                      x1="10"
+                      y1="7"
+                      x2="10"
+                      y2="15"
+                      gradientUnits="userSpaceOnUse"
+                    >
+                      <stop stop-color="#9EF01A" />
+                      <stop offset="0.84375" stop-color="#08DA08" />
+                      <stop offset="1" stop-color="#09DA09" />
+                    </linearGradient>
+                    <linearGradient
+                      id="paint2_linear_17_339"
+                      x1="10"
+                      y1="1"
+                      x2="10"
+                      y2="3"
+                      gradientUnits="userSpaceOnUse"
+                    >
+                      <stop stop-color="#9EF01A" />
+                      <stop offset="0.84375" stop-color="#08DA08" />
+                      <stop offset="1" stop-color="#09DA09" />
+                    </linearGradient>
+                    <linearGradient
+                      id="paint3_linear_17_339"
+                      x1="17"
+                      y1="4"
+                      x2="17"
+                      y2="6"
+                      gradientUnits="userSpaceOnUse"
+                    >
+                      <stop stop-color="#9EF01A" />
+                      <stop offset="0.84375" stop-color="#08DA08" />
+                      <stop offset="1" stop-color="#09DA09" />
+                    </linearGradient>
+                  </defs>
+                </svg>
+                <p>Print</p>
+              </div>
+            </div>
+            <p className="text-sm text-paragraph">
+              This bridge, which acts as the heart of economic city Butwal, is
+              the mega project announced recently. Lets see its environment
+              analysis.
+            </p>
+            <div className="w-full px-4 py-4  flex flex-col justify-betweenpy-3 border h-full rounded-md mt-4 ">
+              <p className="text-md  text-dark font-semibold">
+                Basic Details
+              </p>
+              <p className="text-sm mt-5 text-paragraph font-medium">
+                Project Type: <span className="text-dark font-semibold">Bridge</span>
+              </p>
+              <p className="text-sm mt-2 text-paragraph font-medium">
+                Coordinates: <span className="text-dark font-semibold">-36.33 E, 86.99N</span>
+              </p>
+              <p className="text-sm mt-2 text-paragraph font-medium">
+                Area: <span className="text-dark font-semibold">1600 Sq. Km</span>
+              </p>
+             
+             
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default stats
+export default stats;
